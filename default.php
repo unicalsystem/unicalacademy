a86adec850f51ea39f0ea62bf58bb65ff869158d
unicalsystem <116626069+unicalsystem@users.noreply.github.com>
2023-01-18 18:11:57 +0530

    default.php
    Sample edit

--- default.php
+++ default.php
@@ -162,7 +162,7 @@
                     rel="nofollow"
                     target="_blank"
                 >
-                    How can I migrate a website to Hostinger? 
+                    How can I migrate a website to Unical? 
                     <svg class="ic-launch" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path 
                             fill-rule="evenodd" 
